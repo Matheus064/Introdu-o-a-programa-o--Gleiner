537ca418fe4b6a7676b1c3b33fd10004696ab719
@matheus_064 <matheusjose2312@gmail.com>
2025-08-19 08:45:44 -0300

    Add files via upload

---
+++ for.py
@@ -0,0 +1,8 @@
+print("Esta aplicação mostre a tabuada de 1 a 10  de um número escolhido !")
+numero=int(input("Digite um número: "))
+for i in range(10):
+ print(numero,"x",i+1,"=",numero*(i+1))
+
+
+
+
