51d0725e68d839f351598245d4462b56ed76151e
@matheus_064 <matheusjose2312@gmail.com>
2025-08-21 10:37:44 -0300

    Add files via upload

---
+++ NOTAQUEFALTA.py
@@ -0,0 +1,5 @@
+nota1= float(input("Digite a sua nota do 1° Bimestre: "))
+nota2= float(input("Digite a sua nota do 2° Bimestre: "))
+media= ((60 - (nota1*2+nota2*2))/2)
+nota3_4= media/3
+print("Ele precisará tirar essa nota nos dois próximos bimestres: ", nota3_4)    
