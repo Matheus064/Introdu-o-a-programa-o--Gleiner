5b1c2261f1181580668d7e173bac7c4888e86f3a
@matheus_064 <matheusjose2312@gmail.com>
2025-11-25 08:51:37 -0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,10 @@
+opção5=""
+while opção5 != "sair":
+      print(" opição:1\n opicão:2\n opção:3\n opção:4\n opção:5\n sair")
+      opção5=input("Digite a opção desejada: ")
+
+
+
+
+
+
---
+++ senha= 1234.py
@@ -0,0 +1,13 @@
+sen= 1234
+while   True:
+    senha_input = int(input("Digite a senha: "))
+    if senha_input == sen:
+        print("Acesso permitido.")
+    else:
+        print("Senha incorreta. Tente novamente.")  
+
+
+
+
+
+ 
